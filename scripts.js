c5e333368dab8770003f94ad3f7bb7a9f9fe9946
revmc <shaque9000@gmail.com>
2016-10-24 23:52:54 -0400

    Adding some animation to the toolbar and modifying some styling

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log('testing');
+var slideInNav = document.querySelector('.toolbar');
+
+slideInNav.addEventListener('mouseenter', function() {
+	slideInNav.className += ' extended';
+});
+
+slideInNav.addEventListener('mouseleave', function() {
+	slideInNav.className = slideInNav.className.replace
+      ( /(?:^|\s)extended(?!\S)/g , '' )
+});
+console.log('slideInNav: ', slideInNav);
